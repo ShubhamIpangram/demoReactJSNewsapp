fbe0d0a4d08d25f008489d26342b1965001d9b2c
ShubhamIpangram <shubhamshukla@ipangram.com>
2022-11-04 14:54:37 +0530

    setPagination

---
+++ newsapp/src/components/NewsItem.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+    render() {
+        let { title, description, imageUrl, newsUrl } = this.props;
+        return (
+            <div className="my-3">
+                <div className="card" style={{ width: "18rem" }}>
+                    <img className="card-img-top" src={!imageUrl ? "https://cdn.vox-cdn.com/thumbor/lh2YBh8cWvn3ARrenXxjolBte4o=/0x0:2040x1360/1200x628/filters:focal(1020x680:1021x681)/cdn.vox-cdn.com/uploads/chorus_asset/file/23932655/acastro_STK106__01.jpg" : imageUrl} alt="Card image cap" />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}...</h5>
+                        <p className="card-text">{description}...</p>
+                        <a href={newsUrl} target="_blank" className="btn  btn-sm btn-dark">Read More</a>
+                    </div>
+                </div>
+            </div >
+        )
+    }
+}
+
+export default NewsItem

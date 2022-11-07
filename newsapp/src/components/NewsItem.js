01ba9f6faf810cefc0e01a536ac2c93bc7275e80
ShubhamIpangram <shubhamshukla@ipangram.com>
2022-11-07 14:09:39 +0530

    letestCode

--- newsapp/src/components/NewsItem.js
+++ newsapp/src/components/NewsItem.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
     render() {
-        let { title, description, imageUrl, newsUrl } = this.props;
+        let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
         return (
             <div className="my-3">
                 <div className="card">
+                    <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{ left: '90', zIndex: 1 }}>{source}</span>
                     <img className="card-img-top" src={!imageUrl ? "https://cdn.vox-cdn.com/thumbor/lh2YBh8cWvn3ARrenXxjolBte4o=/0x0:2040x1360/1200x628/filters:focal(1020x680:1021x681)/cdn.vox-cdn.com/uploads/chorus_asset/file/23932655/acastro_STK106__01.jpg" : imageUrl} alt="Card image cap" />
                     <div className="card-body">
-                        <h5 className="card-title">{title}...</h5>
+                        <h5 className="card-title">{title} </h5>
                         <p className="card-text">{description}...</p>
+                        <p className="card-text"><small className="text-muted"> By {!author ? "Unknow" : author} on {new Date(date).toGMTString()}</small></p>
                         <a href={newsUrl} target="_blank" className="btn  btn-sm btn-dark">Read More</a>
                     </div>
                 </div>

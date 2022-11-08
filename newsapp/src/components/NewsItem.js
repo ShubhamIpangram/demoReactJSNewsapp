749037a76496384114c6563aa5e912da274c631d
ShubhamIpangram <shubhamshukla@ipangram.com>
2022-11-08 09:33:08 +0530

    letestCode

--- newsapp/src/components/NewsItem.js
+++ newsapp/src/components/NewsItem.js
@@ -6,7 +6,15 @@ export class NewsItem extends Component {
         return (
             <div className="my-3">
                 <div className="card">
-                    <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{ left: '90', zIndex: 1 }}>{source}</span>
+                    <div style={{
+                        display: 'flex',
+                        justifyContent: 'flex-end',
+                        position: 'absolute',
+                        right: '0'
+                    }}>
+                        <span className="badge rounded-pill bg-danger">{source}</span>
+                    </div>
+
                     <img className="card-img-top" src={!imageUrl ? "https://cdn.vox-cdn.com/thumbor/lh2YBh8cWvn3ARrenXxjolBte4o=/0x0:2040x1360/1200x628/filters:focal(1020x680:1021x681)/cdn.vox-cdn.com/uploads/chorus_asset/file/23932655/acastro_STK106__01.jpg" : imageUrl} alt="Card image cap" />
                     <div className="card-body">
                         <h5 className="card-title">{title} </h5>

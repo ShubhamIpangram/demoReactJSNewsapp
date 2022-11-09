ac8aefd6e674b0310287dc1669d5050e6c4e960d
ShubhamIpangram <shubhamshukla@ipangram.com>
2022-11-09 15:42:15 +0530

    letestCode

--- newsapp/src/components/NavBar.js
+++ newsapp/src/components/NavBar.js
@@ -1,35 +1,32 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class Navbar extends Component {
+const Navbar = () => {
+    return (
+        <div>
+            <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+                <a className="navbar-brand" href="/">News</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
 
-    render() {
-        return (
-            <div>
-                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                    <a className="navbar-brand" href="/">News</a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item"> <a className="nav-link" href="/business">Business</a></li>
+                        <li className="nav-item"> <a className="nav-link" href="/entertainment">Entertainment</a></li>
+                        <li className="nav-item"> <a className="nav-link" href="/general">General</a></li>
+                        <li className="nav-item"><a className="nav-link" href="/health">Health</a></li>
+                        <li className="nav-item"><a className="nav-link" href="/science">Science</a></li>
+                        <li className="nav-item"><a className="nav-link" href="/sports">Sports</a></li>
+                        <li className="nav-item"> <a className="nav-link" href="/technology">Technology</a> </li>
+                    </ul>
 
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav mr-auto">
-                            <li className="nav-item active">
-                                <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
-                            </li>
-                            <li className="nav-item"> <a className="nav-link" href="/business">Business</a></li>
-                            <li className="nav-item"> <a className="nav-link" href="/entertainment">Entertainment</a></li>
-                            <li className="nav-item"> <a className="nav-link" href="/general">General</a></li>
-                            <li className="nav-item"><a className="nav-link" href="/health">Health</a></li>
-                            <li className="nav-item"><a className="nav-link" href="/science">Science</a></li>
-                            <li className="nav-item"><a className="nav-link" href="/sports">Sports</a></li>
-                            <li className="nav-item"> <a className="nav-link" href="/technology">Technology</a> </li>
-                        </ul>
-
-                    </div>
-                </nav>
-            </div>
-        )
-    }
+                </div>
+            </nav>
+        </div>
+    )
 }
 
 export default Navbar
--- newsapp/src/components/NewsItem.js
+++ newsapp/src/components/NewsItem.js
@@ -1,31 +1,30 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class NewsItem extends Component {
-    render() {
-        let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
-        return (
-            <div className="my-3">
-                <div className="card">
-                    <div style={{
-                        display: 'flex',
-                        justifyContent: 'flex-end',
-                        position: 'absolute',
-                        right: '0'
-                    }}>
-                        <span className="badge rounded-pill bg-danger">{source}</span>
-                    </div>
+const NewsItem = (props) => {
 
-                    <img className="card-img-top" src={!imageUrl ? "https://cdn.vox-cdn.com/thumbor/lh2YBh8cWvn3ARrenXxjolBte4o=/0x0:2040x1360/1200x628/filters:focal(1020x680:1021x681)/cdn.vox-cdn.com/uploads/chorus_asset/file/23932655/acastro_STK106__01.jpg" : imageUrl} alt="Card image cap" />
-                    <div className="card-body">
-                        <h5 className="card-title">{title} </h5>
-                        <p className="card-text">{description}...</p>
-                        <p className="card-text"><small className="text-muted"> By {!author ? "Unknow" : author} on {new Date(date).toGMTString()}</small></p>
-                        <a href={newsUrl} target="_blank" className="btn  btn-sm btn-dark">Read More</a>
-                    </div>
+    let { title, description, imageUrl, newsUrl, author, date, source } = props;
+    return (
+        <div className="my-3">
+            <div className="card">
+                <div style={{
+                    display: 'flex',
+                    justifyContent: 'flex-end',
+                    position: 'absolute',
+                    right: '0'
+                }}>
+                    <span className="badge rounded-pill bg-danger">{source}</span>
                 </div>
-            </div >
-        )
-    }
+
+                <img className="card-img-top" src={!imageUrl ? "https://cdn.vox-cdn.com/thumbor/lh2YBh8cWvn3ARrenXxjolBte4o=/0x0:2040x1360/1200x628/filters:focal(1020x680:1021x681)/cdn.vox-cdn.com/uploads/chorus_asset/file/23932655/acastro_STK106__01.jpg" : imageUrl} alt="Card image cap" />
+                <div className="card-body">
+                    <h5 className="card-title">{title} </h5>
+                    <p className="card-text">{description}...</p>
+                    <p className="card-text"><small className="text-muted"> By {!author ? "Unknow" : author} on {new Date(date).toGMTString()}</small></p>
+                    <a href={newsUrl} target="_blank" className="btn  btn-sm btn-dark">Read More</a>
+                </div>
+            </div>
+        </div >
+    )
 }
 
 export default NewsItem
